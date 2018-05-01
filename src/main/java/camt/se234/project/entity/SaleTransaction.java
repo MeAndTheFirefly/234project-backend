7ddfa32e388ba62c6452906ffa9ccd7a1a492576
GUOQING LI <leevn2011@hotmail.com>
2018-05-01 20:43:11 +0700

    generate getter setters for SaleTransacation and User

--- src/main/java/camt/se234/project/entity/SaleTransaction.java
+++ src/main/java/camt/se234/project/entity/SaleTransaction.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
 
@@ -25,4 +24,44 @@ public class SaleTransaction {
     @OneToOne (fetch = FetchType.EAGER)
     Product product;
     int amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public SaleOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(SaleOrder order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
