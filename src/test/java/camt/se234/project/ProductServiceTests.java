f4eea6d173b05939d8b8e5369ddec67c7d12a1b2
James Taylor <jt+cmu@sent.com>
2018-05-07 00:29:11 +0700

    new mock data for ProductServiceTests

--- src/test/java/camt/se234/project/ProductServiceTests.java
+++ src/test/java/camt/se234/project/ProductServiceTests.java
@@ -47,12 +47,12 @@ public class ProductServiceTests {
 		ProductDao productDao = mock(ProductDao.class);
 		((ProductServiceImpl) productSrv).setProductDao(productDao);
 		List<Product> mockProducts = new ArrayList<Product>();
-		mockProducts.add(new Product("p0001", "Garden", "The garden which you can grow everything on earth", "http://localhost:8080/images/garden.jpg", 20000));
-		mockProducts.add(new Product("p0002", "Banana", "A good fruit with very cheap price", "http://localhost:8080/images/banana.jpg", 150));
-		mockProducts.add(new Product("p0003", "Orange", "Nothing good about it", "http://localhost:8080/images/orange.jpg", 280));
-		mockProducts.add(new Product("p0004", "Papaya", "Use for papaya salad", "http://localhost:8080/images/papaya.jpg", 12));
-		mockProducts.add(new Product("p0005", "Rambutan", "An expensive fruit from the sout", "http://localhost:8080/images/rambutan.jpg", 20));
-		mockProducts.add(new Product("p0006", "Unknow", "Don't use this", "http://localhost:8080/images/rambutan.jpg", -1));
+		mockProducts.add(new Product("p01", "Den", "My garden den", "http://localhost:8080/images/p01.jpg", 11));
+		mockProducts.add(new Product("p02", "Ban", "Thai house", "http://localhost:8080/images/p02.jpg", 22));
+		mockProducts.add(new Product("p03", "Org", "Nasty fruit", "http://localhost:8080/images/p03.jpg", 33));
+		mockProducts.add(new Product("p04", "Pap", "Nice fruit", "http://localhost:8080/images/p04.jpg", 44));
+		mockProducts.add(new Product("p05", "Ram", "Southern fruit", "http://localhost:8080/images/p05.jpg", 55));
+		mockProducts.add(new Product("p06", "Oop", "My mistake", "http://localhost:8080/images/p06.jpg", -1));
 		when(productDao.getProducts()).thenReturn(mockProducts);
 	}
 
@@ -60,23 +60,23 @@ public class ProductServiceTests {
 	public void testGetAllProdMock() {
 		List<Product> list = productSrv.getAllProducts();
 		assertThat(list.size(), is(6));
-		assertThat(list.get(0), is(new Product("p0001", "Garden", "The garden which you can grow everything on earth", "http://localhost:8080/images/garden.jpg", 20000)));
-		assertThat(list.get(1), is(new Product("p0002", "Banana", "A good fruit with very cheap price", "http://localhost:8080/images/banana.jpg", 150)));
-		assertThat(list.get(2), is(new Product("p0003", "Orange", "Nothing good about it", "http://localhost:8080/images/orange.jpg", 280)));
-		assertThat(list.get(3), is(new Product("p0004", "Papaya", "Use for papaya salad", "http://localhost:8080/images/papaya.jpg", 12)));
-		assertThat(list.get(4), is(new Product("p0005", "Rambutan", "An expensive fruit from the sout", "http://localhost:8080/images/rambutan.jpg", 20)));
-		assertThat(list.get(5), is(new Product("p0006", "Unknow", "Don't use this", "http://localhost:8080/images/rambutan.jpg", -1)));
+		assertThat(list.get(0), is(new Product("p01", "Den", "My garden den", "http://localhost:8080/images/p01.jpg", 11)));
+		assertThat(list.get(1), is(new Product("p02", "Ban", "Thai house", "http://localhost:8080/images/p02.jpg", 22)));
+		assertThat(list.get(2), is(new Product("p03", "Org", "Nasty fruit", "http://localhost:8080/images/p03.jpg", 33)));
+		assertThat(list.get(3), is(new Product("p04", "Pap", "Nice fruit", "http://localhost:8080/images/p04.jpg", 44)));
+		assertThat(list.get(4), is(new Product("p05", "Ram", "Southern fruit", "http://localhost:8080/images/p05.jpg", 55)));
+		assertThat(list.get(5), is(new Product("p06", "Oop", "My mistake", "http://localhost:8080/images/p06.jpg", -1)));
 	}
 
 	@Test
 	public void testGetAvailProdMock() {
 		List<Product> list = productSrv.getAvailableProducts();
 		assertThat(list.size(), is(5));
-		assertThat(list.get(0), is(new Product("p0001", "Garden", "The garden which you can grow everything on earth", "http://localhost:8080/images/garden.jpg", 20000)));
-		assertThat(list.get(1), is(new Product("p0002", "Banana", "A good fruit with very cheap price", "http://localhost:8080/images/banana.jpg", 150)));
-		assertThat(list.get(2), is(new Product("p0003", "Orange", "Nothing good about it", "http://localhost:8080/images/orange.jpg", 280)));
-		assertThat(list.get(3), is(new Product("p0004", "Papaya", "Use for papaya salad", "http://localhost:8080/images/papaya.jpg", 12)));
-		assertThat(list.get(4), is(new Product("p0005", "Rambutan", "An expensive fruit from the sout", "http://localhost:8080/images/rambutan.jpg", 20)));
+		assertThat(list.get(0), is(new Product("p01", "Den", "My garden den", "http://localhost:8080/images/p01.jpg", 11)));
+		assertThat(list.get(1), is(new Product("p02", "Ban", "Thai house", "http://localhost:8080/images/p02.jpg", 22)));
+		assertThat(list.get(2), is(new Product("p03", "Org", "Nasty fruit", "http://localhost:8080/images/p03.jpg", 33)));
+		assertThat(list.get(3), is(new Product("p04", "Pap", "Nice fruit", "http://localhost:8080/images/p04.jpg", 44)));
+		assertThat(list.get(4), is(new Product("p05", "Ram", "Southern fruit", "http://localhost:8080/images/p05.jpg", 55)));
 	}
 
 	@Test
