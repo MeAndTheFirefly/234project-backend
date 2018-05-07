5394ea8a257dbc31b390a806d695f08d4d5147be
GUOQING LI <leevn2011@hotmail.com>
2018-05-07 10:13:17 +0700

    add tests for get-avaiable and unavaiable products with xlsx data

--- src/test/java/camt/se234/project/ProductServiceTests.java
+++ src/test/java/camt/se234/project/ProductServiceTests.java
@@ -29,21 +29,49 @@ public class ProductServiceTests {
 	ProductService productSrv;
 
 	@Test
-	public void testProductSrv() {
+	public void testProductSrv() { // test products with the data from xlsx file.
 		assertThat(productSrv, is(notNullValue()));
 		List<Product> products = productSrv.getAllProducts();
 		assertThat(products, is(notNullValue()));
 		assertThat(products.size(), is(6));
-		assertThat(products.get(0), is(new Product("p0001", "Garden", "The garden which you can grow everything on earth", "http://localhost:8080/images/garden.jpg", 20000)));
-		assertThat(products.get(1), is(new Product("p0002", "Banana", "A good fruit with very cheap price", "http://localhost:8080/images/banana.jpg", 150)));
-		assertThat(products.get(2), is(new Product("p0003", "Orange", "Nothing good about it", "http://localhost:8080/images/orange.jpg", 280)));
-		assertThat(products.get(3), is(new Product("p0004", "Papaya", "Use for papaya salad", "http://localhost:8080/images/papaya.jpg", 12)));
-		assertThat(products.get(4), is(new Product("p0005", "Rambutan", "An expensive fruit from the sout", "http://localhost:8080/images/rambutan.jpg", 20)));
-		assertThat(products.get(5), is(new Product("p0006", "Unknow", "Don't use this", "http://localhost:8080/images/rambutan.jpg", -1)));
+		assertThat(products.get(0),
+				is(new Product("p0001", "Garden", "The garden which you can grow everything on earth",
+						"http://localhost:8080/images/garden.jpg", 20000)));
+		assertThat(products.get(1), is(new Product("p0002", "Banana", "A good fruit with very cheap price",
+				"http://localhost:8080/images/banana.jpg", 150)));
+		assertThat(products.get(2), is(new Product("p0003", "Orange", "Nothing good about it",
+				"http://localhost:8080/images/orange.jpg", 280)));
+		assertThat(products.get(3), is(
+				new Product("p0004", "Papaya", "Use for papaya salad", "http://localhost:8080/images/papaya.jpg", 12)));
+		assertThat(products.get(4), is(new Product("p0005", "Rambutan", "An expensive fruit from the sout",
+				"http://localhost:8080/images/rambutan.jpg", 20)));
+		assertThat(products.get(5),
+				is(new Product("p0006", "Unknow", "Don't use this", "http://localhost:8080/images/rambutan.jpg", -1)));
 	}
 
 	@Test
-	public void mockSetup() {
+	public void testGetAvailProd() { // test getAvailableproducts with the data from xlsx file.
+		List<Product> list = productSrv.getAvailableProducts();
+		assertThat(list.size(), is(5));
+		assertThat(list.get(0), is(new Product("p0001", "Garden", "The garden which you can grow everything on earth",
+				"http://localhost:8080/images/garden.jpg", 20000)));
+		assertThat(list.get(1), is(new Product("p0002", "Banana", "A good fruit with very cheap price",
+				"http://localhost:8080/images/banana.jpg", 150)));
+		assertThat(list.get(2), is(new Product("p0003", "Orange", "Nothing good about it",
+				"http://localhost:8080/images/orange.jpg", 280)));
+		assertThat(list.get(3), is(
+				new Product("p0004", "Papaya", "Use for papaya salad", "http://localhost:8080/images/papaya.jpg", 12)));
+		assertThat(list.get(4), is(new Product("p0005", "Rambutan", "An expensive fruit from the sout",
+				"http://localhost:8080/images/rambutan.jpg", 20)));
+	}
+
+	@Test
+	public void testGetUnavailProd() { // test getUnavailableproducts with the data from xlsx file.
+		assertThat(productSrv.getUnavailableProductSize(), is(1));
+	}
+
+	@Test
+	public void mockSetup() { // initial set up for mock data.
 		ProductDao productDao = mock(ProductDao.class);
 		((ProductServiceImpl) productSrv).setProductDao(productDao);
 		List<Product> mockProducts = new ArrayList<Product>();
@@ -57,34 +85,41 @@ public class ProductServiceTests {
 	}
 
 	@Test
-	public void testGetAllProdMock() {
+	public void testGetAllProdMock() { // test get all products with mock data.
 		List<Product> list = productSrv.getAllProducts();
 		assertThat(list.size(), is(6));
-		assertThat(list.get(0), is(new Product("p01", "Den", "My garden den", "http://localhost:8080/images/p01.jpg", 11)));
-		assertThat(list.get(1), is(new Product("p02", "Ban", "Thai house", "http://localhost:8080/images/p02.jpg", 22)));
-		assertThat(list.get(2), is(new Product("p03", "Org", "Nasty fruit", "http://localhost:8080/images/p03.jpg", 33)));
-		assertThat(list.get(3), is(new Product("p04", "Pap", "Nice fruit", "http://localhost:8080/images/p04.jpg", 44)));
-		assertThat(list.get(4), is(new Product("p05", "Ram", "Southern fruit", "http://localhost:8080/images/p05.jpg", 55)));
-		assertThat(list.get(5), is(new Product("p06", "Oop", "My mistake", "http://localhost:8080/images/p06.jpg", -1)));
+		assertThat(list.get(0),
+				is(new Product("p01", "Den", "My garden den", "http://localhost:8080/images/p01.jpg", 11)));
+		assertThat(list.get(1),
+				is(new Product("p02", "Ban", "Thai house", "http://localhost:8080/images/p02.jpg", 22)));
+		assertThat(list.get(2),
+				is(new Product("p03", "Org", "Nasty fruit", "http://localhost:8080/images/p03.jpg", 33)));
+		assertThat(list.get(3),
+				is(new Product("p04", "Pap", "Nice fruit", "http://localhost:8080/images/p04.jpg", 44)));
+		assertThat(list.get(4),
+				is(new Product("p05", "Ram", "Southern fruit", "http://localhost:8080/images/p05.jpg", 55)));
+		assertThat(list.get(5),
+				is(new Product("p06", "Oop", "My mistake", "http://localhost:8080/images/p06.jpg", -1)));
 	}
 
 	@Test
-	public void testGetAvailProdMock() {
+	public void testGetAvailProdMock() { // test getAvailableproducts with mock data.
 		List<Product> list = productSrv.getAvailableProducts();
 		assertThat(list.size(), is(5));
-		assertThat(list.get(0), is(new Product("p01", "Den", "My garden den", "http://localhost:8080/images/p01.jpg", 11)));
-		assertThat(list.get(1), is(new Product("p02", "Ban", "Thai house", "http://localhost:8080/images/p02.jpg", 22)));
-		assertThat(list.get(2), is(new Product("p03", "Org", "Nasty fruit", "http://localhost:8080/images/p03.jpg", 33)));
-		assertThat(list.get(3), is(new Product("p04", "Pap", "Nice fruit", "http://localhost:8080/images/p04.jpg", 44)));
-		assertThat(list.get(4), is(new Product("p05", "Ram", "Southern fruit", "http://localhost:8080/images/p05.jpg", 55)));
+		assertThat(list.get(0),
+				is(new Product("p01", "Den", "My garden den", "http://localhost:8080/images/p01.jpg", 11)));
+		assertThat(list.get(1),
+				is(new Product("p02", "Ban", "Thai house", "http://localhost:8080/images/p02.jpg", 22)));
+		assertThat(list.get(2),
+				is(new Product("p03", "Org", "Nasty fruit", "http://localhost:8080/images/p03.jpg", 33)));
+		assertThat(list.get(3),
+				is(new Product("p04", "Pap", "Nice fruit", "http://localhost:8080/images/p04.jpg", 44)));
+		assertThat(list.get(4),
+				is(new Product("p05", "Ram", "Southern fruit", "http://localhost:8080/images/p05.jpg", 55)));
 	}
 
 	@Test
-	public void testGetUnavailProdMock() {
+	public void testGetUnavailProdMock() { // test getUnavailableproducts with mock data.
 		assertThat(productSrv.getUnavailableProductSize(), is(1));
 	}
 }
-
-
-
-
