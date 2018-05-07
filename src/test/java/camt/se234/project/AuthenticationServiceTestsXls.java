64e8b2b8eb2ada3a20b171879485610937005b23
GUOQING LI <leevn2011@hotmail.com>
2018-05-07 10:56:29 +0700

    remove unused imports

--- src/test/java/camt/se234/project/AuthenticationServiceTestsXls.java
+++ src/test/java/camt/se234/project/AuthenticationServiceTestsXls.java
@@ -7,17 +7,12 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.core.IsNull.nullValue;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import camt.se234.project.dao.UserDao;
 import camt.se234.project.entity.User;
 import camt.se234.project.service.AuthenticationService;
-import camt.se234.project.service.AuthenticationServiceImpl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
