d504422693589e7c3855f3d750ffa84ca67e62bd
GUOQING LI <leevn2011@hotmail.com>
2018-05-09 13:53:36 +0700

    final katalon test report for Aj Dto

--- katalon/Test Suites/Data-Driven Tests Suite.ts
+++ katalon/Test Suites/Data-Driven Tests Suite.ts
@@ -4,7 +4,7 @@
    <name>Data-Driven Tests Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-09T12:18:29</lastRun>
+   <lastRun>2018-05-09T13:45:54</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
